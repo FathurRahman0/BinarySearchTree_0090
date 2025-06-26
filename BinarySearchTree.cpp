180d255c6e2ca232688355645b2be2e4f29f957b
FathurRahman0 <rahmanfthr165@gmail.com>
2025-06-26 18:38:11 +0700

    Step 6 : If the value in the data field of ne node is less than that of parent

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -56,5 +56,16 @@ public:
             // 5b : exit
             return;
         }
+    
+        // Step 6 : If the value in the data field of ne node is less than that of parent
+        if (x < parent->info)
+        {
+            // 6a : make the left child of parent point to the new node
+            parent->leftchild = newNode:
+
+            // 6b : Exit
+            return;
+        }
+    
     }
 }
