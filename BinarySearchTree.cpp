f412bd1466995f4cfe5977451d0e51b531433672
FathurRahman0 <rahmanfthr165@gmail.com>
2025-06-26 18:18:45 +0700

    membuat class node

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+class Node
+{
+public:
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    // Constructor for the node class
+    Node()
+    {
+        leftchild = nullptr; // Intialize left child to null
+        rightchild = nullptr; // Intialize right child to null
+    }
+};
